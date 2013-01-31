e7727fdab8d6e660f1d69e25585142e7607ec7fc
Nsiemer <nsiemer31@gmail.com>
2013-01-31 15:57:48 -0600

    Cleaned up. Still "very sophisticated."

--- problem_006.rb
+++ problem_006.rb
@@ -1,14 +1,6 @@
 # Find the difference between the sum of the squares of the first one hundred natural numbers 
 # and the square of the sum.
 
-ary1 = []
-for i in 1..100
-	ary1.push(i)
-end
-ary2 = ary1
-# First squares each number then finds the sum via inject
-sum_of_squares = ary1.collect{|x| x ** 2}.inject(0){|sum, item| sum + item}
-# only finds the sum 
-square_of_sum = ary2.inject(0){|sum, item| sum + item}
-# BOOM. This one's tough keep up with, but ary2 sum is squared AND subtracted from.
+sum_of_squares = (1..100).collect{|x| x ** 2}.inject(0){|sum, item| sum + item}
+square_of_sum = (1..100).inject(0){|sum, item| sum + item}
 puts (square_of_sum ** 2) - sum_of_squares
