d389006aa26d00b9c499c4eda3b61f9c1ecee82d
Nsiemer <nsiemer31@gmail.com>
2013-02-07 15:13:11 -0600

    refactored

--- problem_024.rb
+++ problem_024.rb
@@ -1,4 +1,1 @@
-# Problem 24
-# What is the millionth lexicographic permutation of the digits 0, 1, 2, 3, 4, 5, 6, 7, 8 and 9?
-
-puts "#{[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].permutation.sort[999_999].join""}"
+puts (0..9).to_a.permutation.sort[999_999].join
